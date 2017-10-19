f729499c838f62cb9bda09b1c6dba530a492a651
agiko <agiko2@o2.pl>
2017-10-19 17:49:32 +0200

    program trzeci

---
+++ program2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+
+
+
+int main() 
+{
+
+	putchar('V'); 
+	putchar('\n'); /* znak konca lini */
+	puts ("wypisz string / ciag znakow");
+	puts ("\nAla ma kota,\na kot ma Ale");
+
+	return 0;
+
+}
+
+
